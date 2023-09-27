ccfc284b6cc113bfbe28f0e8f14354d677c59988
rnathiya <146078053+rnathiya@users.noreply.github.com>
2023-09-27 11:21:21 +0530

    Update challenge 1.1.py

--- challenge 1.1.py
+++ challenge 1.1.py
@@ -1,12 +1,16 @@
- # Python program to check if year is a leap year or not
+ # Factorial of a number using recursion
 
-year = 6032
+def recur_factorial(n):
+   if n == 1:
+       return n
+   else:
+       return n*recur_factorial(n-1)
 
-if (year % 400 == 0) and (year % 100 == 0):
-    print("{0} is a leap year".format(year))
+num = 8
 
-elif (year % 4 ==0) and (year % 100 != 0):
-    print("{0} is a leap year".format(year))
-          
+if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
 else:
-    print("{0} is not a leap year".format(year))
+   print("The factorial of", num, "is", recur_factorial(num))
